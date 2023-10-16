0d72b279c66153ff1e4d8811b4311e8221d0d2b3
Carolina Freitas <99994934+CarolinaSFreitas@users.noreply.github.com>
2023-10-16 09:50:30 -0300

    Add files via upload

--- exercicios-conjuntos-nomes/nomes.py
+++ exercicios-conjuntos-nomes/nomes.py
@@ -6,6 +6,60 @@ def titulo(texto, sublinhado="-"):
     print(texto)
     print(sublinhado*40)
 
+
+def inclusao():
+    titulo("Inclusão dos Nomes para Análise")
+
+    while True:
+        aux1 = input("1º Nome: ")
+        #strip() retira os espaços no inicio e final da string
+        if " " in aux1.strip():
+            break
+        else:
+            print("Por favor, digite um nome completo :S")
+
+    while True:
+        aux2 = input("2º Nome: ")
+        #strip() retira os espaços no inicio e final da string
+        if " " in aux2.strip():
+            break
+        else:
+            print("Por favor, digite um nome completo :S")
+
+
+    #desta forma nao precisa o global, porque a inclusao só pode ocorrer no conjunto (nome1) ja declarado 
+   #for letra in aux1:
+    #    nome1.add(letra)
+
+    global nome1
+    global nome2
+    nome1 = set(aux1.lower())
+    nome2 = set(aux2.lower())
+    print("Ok! Nomes cadastrados com sucesso :)")
+
+
+def interseccao():
+    titulo("Intersecção das Letras dos Nomes")
+
+    intersect = nome1.intersection(nome2)
+    print("")
+
+
+
+
+
+
+def diferenca():
+    pass
+
+
+def uniao():
+    pass
+
+
+def alfabeto():
+    pass
+
 #--------------------------------------------------------------------programa principal
 while True:
     titulo("Conjuntos: Manipulação de Letras de Nomes")
